086bc3d5234d8ae1e57d4431e771d7dfbd9b5397
Reeksash <sashreekindukuru2010@gmail.com>
2026-06-11 20:25:18 +0530

    Start and End Screens

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ let shield = false;
 let double = false;
 let magnet = false;
 let clear = false;
+let powerupMessage = '';
+let powerupTimer = 0;
+let personalBest = localStorage.getItem("best") || 0;
 
 function drawPlayer() {
     ctx.fillStyle = "#fff";
@@ -67,7 +70,18 @@ function drawItems() {
 function drawScore() {
     ctx.fillStyle = "#fff"
     ctx.font = 'bold 20px Arial';
+    ctx.textAlign = "left";
     ctx.fillText('Score: ' + score, 10, 30);
+}
+
+function drawPowerupMessage() {
+    if (powerupTimer > 0) {
+        ctx.fillStyle = "#fff";
+        ctx.font = "bold 40px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText(powerupMessage, canvas.width / 2, canvas.height / 2);
+        powerupTimer--;
+    }
 }
 
 function updatePlayer() {
@@ -90,6 +104,8 @@ function updateItems() {
             if (item.type === "shield") {
                 shield = true;
                 items.splice(index, 1);
+                powerupMessage = 'SHIELD ACTIVATED'
+                powerupTimer = 120
                 return;
             }
 
@@ -99,25 +115,45 @@ function updateItems() {
                 return;
             }
 
+            if (item.type === "double") {
+                double = true;
+                powerupMessage = 'DOUBLE POINTS'
+                powerupTimer = 120
+
+                setTimeout(() => {
+                    double = false;
+                }, 10000);
+
+                items.splice(index, 1);
+                return;
+            }
+
+            if (item.type === "bomb") {
+                items.length = 0;
+                powerupMessage = 'OO EXPLOSIONS!'
+                powerupTimer = 120
+                return;
+            }
+
             let points = item.points;
 
             if (double && points > 0) {
                 points *= 2;
             }
-            
 
-            if (item.type === "bomb") {
-                items.length = 0;
-            }
+            score += points;
 
             items.splice(index, 1);
-            score += item.points;
             
             if (score > maxScore) {
                 maxScore = score;
             }
 
             if (score < 0) {
+                if (maxScore > personalBest) {
+                    personalBest = maxScore;
+                    localStorage.setItem("best", personalBest);
+                }
                 gameOver = true;
             }
         }
@@ -133,7 +169,7 @@ function createAsteroid1() {
         y: -20,
         radius: 10,
         dy: 5,
-        color: '#766e6d',
+        color: '#7b7b7b',
         points: -5
 
     })
@@ -145,7 +181,7 @@ function createAsteroid2() {
         y: -20,
         radius: 25,
         dy: 1,
-        color: '#70717e',
+        color: '#7b7b7b',
         points: -5
     })
 }
@@ -156,7 +192,7 @@ function createSpaceDebris() {
         y: -20,
         radius: 20,
         dy: 4,
-        color: '#645d5d',
+        color: '#7b7b7b',
         points: -20
     })
 }
@@ -251,7 +287,73 @@ function createBomb() {
 }
 
 function gameLoop() {
+    if (!gameStarted) {
+        ctx.fillStyle = '#edff29'
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = '#000'
+        ctx.font = 'bold 30px Arial';
+        ctx.textAlign = "center";
+        ctx.fillText('Space Catch', 210, 100);
+
+        ctx.font = 'bold 18px Arial';
+
+        ctx.fillStyle = '#2d2d2d'
+        ctx.textAlign = "center";
+        ctx.fillText('Meteors = -5/-20', 205, 220);
+        ctx.fillStyle = '#ffb702'
+        ctx.textAlign = "center";
+        ctx.fillText('Sun Flare = -2', 205, 250);
+        ctx.fillStyle = '#fb05ff'
+        ctx.textAlign = "center";
+        ctx.fillText('Space Crystals = +5', 205, 280);
+        ctx.fillStyle = '#ffae00'
+        ctx.textAlign = "center";
+        ctx.fillText('Space Gold = +10', 205, 310);
+        ctx.fillStyle = '#21f0d8'
+        ctx.textAlign = "center";
+        ctx.fillText('Star Dust = +3', 205, 340); 
+        ctx.fillStyle = '#455d77'
+        ctx.textAlign = "center";
+        ctx.fillText('Comets = +3', 205, 370);
+        ctx.fillStyle = '#ff0000'
+        ctx.textAlign = "center";
+        ctx.fillText('Shield - Ignores negative point', 205, 400);
+        ctx.fillStyle = '#000dff'
+        ctx.textAlign = "center";
+        ctx.fillText('Double - Twice the points', 205, 430);
+        ctx.fillStyle = '#00ff0d'
+        ctx.textAlign = "center";
+        ctx.fillText('Bomb - Clears all', 205, 460);
+
+        ctx.fillStyle = '#000000'
+        ctx.font = 'bold 20px Arial';
+        ctx.fillText('Press SPACE to Start', 200, 490);
+
+        requestAnimationFrame(gameLoop);
+        return;
+
+    }
+
+    if (gameOver) {
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = '#fff';
+        ctx.font = 'bold 50px Arial'
+        ctx.fillText('GAME OVER', 50, 250);
+
+        ctx.font = '30px Arial'
+        ctx.fillText('Max Score: ' + maxScore, 90, 300)
+        ctx.fillText('Personal Best: ' + personalBest, 90, 340)
+
+        ctx.fillText('Press R to Restart', 65, 380);
+
+        return;
+    }
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 
     updatePlayer();
     updateItems();
@@ -259,6 +361,7 @@ function gameLoop() {
     drawPlayer();
     drawItems();
     drawScore();
+    drawPowerupMessage();
 
     requestAnimationFrame(gameLoop);
 
@@ -267,6 +370,13 @@ function gameLoop() {
 document.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowLeft') player.dx = -5;
     if (e.key === 'ArrowRight') player.dx = 5;
+    if (gameOver && e.key.toLowerCase() === 'r') {
+        location.reload();
+    }
+    if (e.code === 'Space') {
+        gameStarted = true;
+        startSpawning();
+    }
 });
 
 document.addEventListener('keyup', () => {
@@ -285,8 +395,33 @@ function startSpawning() {
     setInterval(createShield, 15000);
     setInterval(createDouble, 20000);
     setInterval(createBomb, 25000);
+
+    setInterval(() => {
+        if (score>= 100) {
+            createAsteroid1();
+            createAsteroid2();
+            createSpaceDebris();
+        }
+
+        if (score>= 150) {
+            createAsteroid1();
+            createAsteroid2();
+            createSpaceDebris();
+        }
+
+        if (score>= 200) {
+            createAsteroid1();
+            createAsteroid2();
+            createSpaceDebris();
+        }
+
+        if (score>= 250) {
+            createAsteroid1();
+            createAsteroid2();
+            createSpaceDebris();
+        }
+    }, 750);
 }
 
 
 gameLoop();
-startSpawning();
